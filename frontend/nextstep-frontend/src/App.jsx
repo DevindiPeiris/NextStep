717537cdfc4148db1f5a40480bea39b77cc8d70b
DevindiPeiris <peirismdp.21@uom.lk>
2025-04-25 16:13:41 +0530

    frotend

---
+++ frontend/nextstep-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import RoleSelection from './pages/RoleSelection'
+
+function App() {
+  
+
+  return (
+    <>
+      <RoleSelection/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/nextstep-frontend/src/pages/RoleSelection.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function RoleSelection() {
+  return (
+  <div>
+    <div className="flex flex-col items-center justify-center mt-50">
+        <h1 className="text-4xl font-bold text-black">Choose Your Role</h1>
+        <p className="text-base text-gray-400 mt-2">Select how you want to join our platform</p>
+    </div>
+  </div>
+    
+
+    
+  );
+}
+
+export default RoleSelection;
+
