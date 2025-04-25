ac6cd876bcbc9257bc2804c9eae0aadec052aef6
DevindiPeiris <peirismdp.21@uom.lk>
2025-04-25 11:21:07 +0530

    SignIn

--- frontend/nextstep-frontend/src/App.js
+++ frontend/nextstep-frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import RoleSelection from './pages/RoleSelection';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
     <div className="App">
       <RoleSelection/>
+      <SignIn/>
     </div>
   );
 }
